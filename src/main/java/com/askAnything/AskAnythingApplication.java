0d90816f373f9f83a2550daed6cae1215b40041a
Han <Han@DESKTOP-Han>
2017-06-09 11:32:26 -0500

    initial commit

---
+++ src/main/java/com/askAnything/AskAnythingApplication.java
@@ -0,0 +1,12 @@
+package com.askAnything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskAnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskAnythingApplication.class, args);
+	}
+}
