8b9deee784b43a6e09145cfb05084d585d99b8be
Han <mhanjung@gmail.com>
2017-06-13 12:25:12 -0500

    Add: H2 database, JPA, URL refactoring, development Profile edit

--- src/main/java/com/askAnything/AskAnythingApplication.java
+++ src/main/java/com/askAnything/AskAnythingApplication.java
@@ -1,12 +1,25 @@
 package com.askAnything;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
+@EntityScan("com.askAnything.domain")
 @SpringBootApplication
 public class AskAnythingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AskAnythingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AskAnythingApplication.class, args);
+  }
+
+  @Bean
+  public ServletRegistrationBean h2servletRegistration() {
+    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+    registration.addUrlMappings("/console/*");
+
+    return registration;
+  }
 }
