38615c14936111544d0d6ab27b6ad65bbd6c83e9
Han <mhanjung@gmail.com>
2017-06-13 20:30:01 -0500

    Add a relationship between Question and User(@ManyToOne)
    and added LocalDateTime column at Question

--- src/main/java/com/askAnything/domain/Question.java
+++ src/main/java/com/askAnything/domain/Question.java
@@ -1,8 +1,15 @@
 package com.askAnything.domain;
 
+import javax.persistence.ForeignKey;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Question {
@@ -10,19 +17,30 @@ public class Question {
   @GeneratedValue
   private Long id;
   
-  private String writer;
+  @ManyToOne
+  @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+  private User writer;
   
   private String title;
   
   private String contents;
   
+  private LocalDateTime createDate;
+  
   public Question(){}
   
-  public Question(String writer, String title, String contents) {
+  public Question(User writer, String title, String contents) {
     super();
     this.writer = writer;
     this.title = title;
     this.contents = contents;
+    this.createDate = LocalDateTime.now();
   }
   
+  public String getFormattedCreateDate(){
+    if(createDate == null){
+      return "";
+    }
+    return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd.HH:mm:ss"));
+  }
 }
